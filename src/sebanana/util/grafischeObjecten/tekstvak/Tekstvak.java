21b717c426077e5629987bd2e1900b04a3b81ae2
Ellen <Ellen@To2By2>
2013-07-22 00:18:54 +0200

    tekstvak uit fxml enzo

--- src/sebanana/util/grafischeObjecten/tekstvak/Tekstvak.java
+++ src/sebanana/util/grafischeObjecten/tekstvak/Tekstvak.java
@@ -1,18 +1,28 @@
 package sebanana.util.grafischeObjecten.tekstvak;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  *
  * @author Ellen
  */
-public class Tekstvak extends HBox {
+public class Tekstvak extends HBox implements Initializable{
     @FXML private Label label;
     @FXML private Label name;
+    @FXML private VBox textbox;
+    //rechterbox keuzeboxxe ofzo toevoegen misschien ofzo
+    @FXML private VBox otherbox;
+    
  
     
     public Tekstvak(){
@@ -30,6 +40,12 @@ public class Tekstvak extends HBox {
         this.getStyleClass().add("hbox");
         
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        makeEmpty();
+        label.setText("welcome !!!");
+    }
     
     
     public void setText(String text){
@@ -43,5 +59,24 @@ public class Tekstvak extends HBox {
     public void makeEmpty(){
         label.setText(null);
         name.setText(null);
+        clearRight();
+    }
+    
+    public void addRight(Node n){
+        otherbox.getChildren().add(n);
+    }
+    
+    public void addAllRight(List<Node> l){
+        for(Node n: l){
+            addRight(n);
+        }
+    }
+    
+    public void removeRight(Node n){
+        otherbox.getChildren().remove(n);
+    }
+    
+    public void clearRight(){
+        otherbox.getChildren().clear();
     }
 }
